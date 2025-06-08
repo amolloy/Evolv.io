d058ff6b0f9c09f0734af410661fe408766c631c
Andy Molloy <amolloy@gmail.com>
2025-06-08 14:14:04 -0400

    Initial Commit

---
+++ Evolv.io/Evolv_ioApp.swift
@@ -0,0 +1,17 @@
+//
+//  Evolv_ioApp.swift
+//  Evolv.io
+//
+//  Created by Andy Molloy on 6/8/25.
+//
+
+import SwiftUI
+
+@main
+struct Evolv_ioApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Evolv.ioTests/Evolv_ioTests.swift
@@ -0,0 +1,16 @@
+//
+//  Evolv_ioTests.swift
+//  Evolv.ioTests
+//
+//  Created by Andy Molloy on 6/8/25.
+//
+
+import Testing
+
+struct Evolv_ioTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
