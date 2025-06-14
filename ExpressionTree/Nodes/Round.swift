2e78f4e1ca5ca02530c07913024786f757bb3e5c
Andy Molloy <amolloy@gmail.com>
2025-06-14 07:44:33 -0400

    alternative round implemntation

--- ExpressionTree/Nodes/Round.swift
+++ ExpressionTree/Nodes/Round.swift
@@ -11,24 +11,24 @@ public class Round: Node {
 	public static var name: String {
 		return "round"
 	}
-
+	
 	public var children: [any Node]
-
+	
 	required public init(_ children: [any Node]) {
 		assert(children.count == 2)
 		self.children = children
 	}
-
+	
 	public func evaluate(using evaluator: Evaluator) -> any ExpressionResult {
 		if let cachedResult = evaluator.result(for: self) {
 			return cachedResult
 		}
-
+		
 		assert(children.count == 2)
 		let result = RoundResult(children.map { $0.evaluate(using: evaluator) })
-
+		
 		evaluator.setResult(result, for: self)
-
+		
 		return result
 	}
 }
@@ -36,17 +36,18 @@ public class Round: Node {
 class RoundResult: ExpressionResult {
 	let e0: ExpressionResult
 	let e1: ExpressionResult
-
+	
 	init(_ es: [ExpressionResult]) {
 		assert(es.count == 2)
 		self.e0 = es[0]
 		self.e1 = es[1]
 	}
-
+	
 	func value(at coord: Coordinate) -> Value {
 		let v0 = e0.value(at: coord)
 		let v1 = e1.value(at: coord)
-
-		return floor(v0 / v1 + 0.5) * v1
+		
+		let q = (v0 / v1).rounded(.toNearestOrAwayFromZero)
+		return q * v1
 	}
 }
