547dba44336f988cedc6f070e28789086c9f2539
Andy Molloy <amolloy@gmail.com>
2026-08-30 12:09:47 -0400

    .toNearestEven matches *Lisp's rounding behavior

--- ExpressionTree/Nodes/Round.swift
+++ ExpressionTree/Nodes/Round.swift
@@ -11,14 +11,14 @@ public class Round: CachedNode {
 	public static var name: String {
 		return "round"
 	}
-	
+
 	public var children: [any Node]
-	
+
 	required public init(_ children: [any Node]) {
 		assert(children.count == 2)
 		self.children = children
 	}
-	
+
 	public func _evaluate(using evaluator: Evaluator) -> any ExpressionResult {
 		assert(children.count == 2)
 		return RoundResult(children.map { $0.evaluate(using: evaluator) })
@@ -28,18 +28,18 @@ public class Round: CachedNode {
 class RoundResult: ExpressionResult {
 	let e0: ExpressionResult
 	let e1: ExpressionResult
-	
+
 	init(_ es: [ExpressionResult]) {
 		assert(es.count == 2)
 		self.e0 = es[0]
 		self.e1 = es[1]
 	}
-	
+
 	func value(at coord: Coordinate) -> Value {
 		let v0 = e0.value(at: coord)
 		let v1 = e1.value(at: coord)
-		
-		let q = (v0 / v1).rounded(.toNearestOrAwayFromZero)
+
+		let q = (v0 / v1).rounded(.toNearestOrEven)
 		return q * v1
 	}
 }
